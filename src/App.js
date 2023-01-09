eb11c65026ceab661d926adcaac2e27328a749c2
asim <107435542+Muhammadasim831@users.noreply.github.com>
2023-01-09 17:58:22 +0500

    Asim first app of react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,155 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+  const [fullname, setfullname] = useState({
+    fname: '',
+    lname: '',
+    email:'',
+    phone:'',
+  });
+  
+
+
+
+  const inputEvent = (event) => {
+    // console.log(event.target.value);
+    // console.log(event.target.name);
+
+    const {value,name} = event.target;
+   
+    
+    setfullname((prevValue)=>{
+      console.log(prevValue);
+
+
+      return {
+      ...prevValue,
+      [name]:value,
+
+      }
+
+
+    // if(name==='fname'){
+    //   return{
+    // fname: value,
+    // lname: prevValue.lname,
+    // email:prevValue.email,
+    // phone:prevValue.phone,
+    //   }
+    //    }
+
+    //    else if(name==='lname'){
+    //     return{
+    //       fname: prevValue.fname,
+    //       lname: value,
+    //       email:prevValue.email,
+    //       phone:prevValue.phone,
+  
+    //     }
+    //      }
+
+    //      else if(name==='lname'){
+    //       return{
+    //         fname: prevValue.fname,
+    //         lname: value,
+    //         email:prevValue.email,
+    //         phone:prevValue.phone,
+    
+    //       }
+    //        }
+
+    //        else if(name==='email'){
+    //         return{
+    //           fname: prevValue.fname,
+    //           lname: prevValue.lname,
+    //           email:value,
+    //           phone:prevValue.phone,
+      
+    //         }
+    //          }
+
+    //          else if(name==='phone'){
+    //           return{
+    //             fname: prevValue.fname,
+    //             lname: prevValue.lname,
+    //             email:prevValue.email,
+    //             phone:value,
+        
+    //           }
+    //            }
+
+
+
+    })
+
+  };
+
+  const onSubmits = (event) => {
+    event.prevebtDefault();
+    alert('form sumitted');
+  };
+
+
+return (
+  <>
+    <form onSubmit={onSubmits}>
+      <div>
+        <h1>Hello {fullname.fname} {fullname.lname}</h1>
+        <p>{fullname.email}</p>
+        <p>{fullname.phone}</p>
+        
+
+
+        <input
+          type="text"
+          Placeholder="Enter your name"
+          name="fname"
+          onChange={inputEvent}
+           value={fullname.fname}
+        />
+          <br></br>
+
+        <input
+          type="text"
+          Placeholder="Enter your last name"
+          name="lname"
+          onChange={inputEvent}
+           value={fullname.lname}
+        />
+          <br></br>
+        <input
+          type="email"
+          Placeholder="Enter your email"
+          name="email"
+          onChange={inputEvent}
+           value={fullname.email}
+           
+        />
+          <br></br>
+
+        <input
+          type="number"
+          Placeholder="Enter your phone  number"
+          name="phone"
+          onChange={inputEvent}
+           value={fullname.phone}
+        />
+          <br></br>
+ 
+       
+        <button
+          type='submit'
+          onClick={onSubmits}>
+          click me
+        </button>
+      </div>
+    </form>
+  </>
+);
+
+};
+
+
 
 export default App;
